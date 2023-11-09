31fe882f5a92a2d1cea59ee5d24deabbcd934196
Steven Clontz <steven.clontz@gmail.com>
2023-11-09 02:29:27 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,230 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "LT1",
+  "level": "1",
+  "url": "LT1.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Limits graphically (LT1)",
+  "body": " Limits graphically (LT1)     Find limits from the graph of a function.      Videos    Video for LT1     Exercises   LT1 Exercises     "
+},
+{
+  "id": "LT1-2",
+  "level": "2",
+  "url": "LT1.html#LT1-2",
+  "type": "Learning Outcomes",
+  "number": "1.1",
+  "title": "",
+  "body": "   Find limits from the graph of a function.    "
+},
+{
+  "id": "LT1-3-2",
+  "level": "2",
+  "url": "LT1.html#LT1-3-2",
+  "type": "Figure",
+  "number": "1",
+  "title": "",
+  "body": "  Video for LT1  "
+},
+{
+  "id": "LT1-4-2",
+  "level": "2",
+  "url": "LT1.html#LT1-4-2",
+  "type": "Figure",
+  "number": "2",
+  "title": "",
+  "body": " LT1 Exercises   "
+},
+{
+  "id": "LT2",
+  "level": "1",
+  "url": "LT2.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Limits numerically (LT2)",
+  "body": " Limits numerically (LT2)     Infer the value of a limit based on nearby values of the function.      Videos    Video for LT2     Exercises   LT2 Exercises     "
+},
+{
+  "id": "LT2-2",
+  "level": "2",
+  "url": "LT2.html#LT2-2",
+  "type": "Learning Outcomes",
+  "number": "1.2",
+  "title": "",
+  "body": "   Infer the value of a limit based on nearby values of the function.    "
+},
+{
+  "id": "LT2-3-2",
+  "level": "2",
+  "url": "LT2.html#LT2-3-2",
+  "type": "Figure",
+  "number": "3",
+  "title": "",
+  "body": "  Video for LT2  "
+},
+{
+  "id": "LT2-4-2",
+  "level": "2",
+  "url": "LT2.html#LT2-4-2",
+  "type": "Figure",
+  "number": "4",
+  "title": "",
+  "body": " LT2 Exercises   "
+},
+{
+  "id": "LT3",
+  "level": "1",
+  "url": "LT3.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Limits analytically (LT3)",
+  "body": " Limits analytically (LT3)     Compute limits of functions given algebraically, using proper limit properties.      Videos    Video for LT3     Exercises   LT3 Exercises     "
+},
+{
+  "id": "LT3-2",
+  "level": "2",
+  "url": "LT3.html#LT3-2",
+  "type": "Learning Outcomes",
+  "number": "1.3",
+  "title": "",
+  "body": "   Compute limits of functions given algebraically, using proper limit properties.    "
+},
+{
+  "id": "LT3-3-2",
+  "level": "2",
+  "url": "LT3.html#LT3-3-2",
+  "type": "Figure",
+  "number": "5",
+  "title": "",
+  "body": "  Video for LT3  "
+},
+{
+  "id": "LT3-4-2",
+  "level": "2",
+  "url": "LT3.html#LT3-4-2",
+  "type": "Figure",
+  "number": "6",
+  "title": "",
+  "body": " LT3 Exercises   "
+},
+{
+  "id": "LT4",
+  "level": "1",
+  "url": "LT4.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Continuity (LT4)",
+  "body": " Continuity (LT4)     Determine where a function is and is not continuous.      Videos    Video for LT4     Exercises   LT4 Exercises     "
+},
+{
+  "id": "LT4-2",
+  "level": "2",
+  "url": "LT4.html#LT4-2",
+  "type": "Learning Outcomes",
+  "number": "1.4",
+  "title": "",
+  "body": "   Determine where a function is and is not continuous.    "
+},
+{
+  "id": "LT4-3-2",
+  "level": "2",
+  "url": "LT4.html#LT4-3-2",
+  "type": "Figure",
+  "number": "7",
+  "title": "",
+  "body": "  Video for LT4  "
+},
+{
+  "id": "LT4-4-2",
+  "level": "2",
+  "url": "LT4.html#LT4-4-2",
+  "type": "Figure",
+  "number": "8",
+  "title": "",
+  "body": " LT4 Exercises   "
+},
+{
+  "id": "LT5",
+  "level": "1",
+  "url": "LT5.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Limits with infinite inputs (LT5)",
+  "body": " Limits with infinite inputs (LT5)     Determine limits of functions at infinity.      Videos    Video for LT5     Exercises   LT5 Exercises     "
+},
+{
+  "id": "LT5-2",
+  "level": "2",
+  "url": "LT5.html#LT5-2",
+  "type": "Learning Outcomes",
+  "number": "1.5",
+  "title": "",
+  "body": "   Determine limits of functions at infinity.    "
+},
+{
+  "id": "LT5-3-2",
+  "level": "2",
+  "url": "LT5.html#LT5-3-2",
+  "type": "Figure",
+  "number": "9",
+  "title": "",
+  "body": "  Video for LT5  "
+},
+{
+  "id": "LT5-4-2",
+  "level": "2",
+  "url": "LT5.html#LT5-4-2",
+  "type": "Figure",
+  "number": "10",
+  "title": "",
+  "body": " LT5 Exercises   "
+},
+{
+  "id": "LT6",
+  "level": "1",
+  "url": "LT6.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Limits with infinite outputs (LT6)",
+  "body": " Limits with infinite outputs (LT6)     Determine limits of functions approaching vertical asymptotes.      Videos    Video for LT6     Exercises   LT6 Exercises     "
+},
+{
+  "id": "LT6-2",
+  "level": "2",
+  "url": "LT6.html#LT6-2",
+  "type": "Learning Outcomes",
+  "number": "1.6",
+  "title": "",
+  "body": "   Determine limits of functions approaching vertical asymptotes.    "
+},
+{
+  "id": "LT6-3-2",
+  "level": "2",
+  "url": "LT6.html#LT6-3-2",
+  "type": "Figure",
+  "number": "11",
+  "title": "",
+  "body": "  Video for LT6  "
+},
+{
+  "id": "LT6-4-2",
+  "level": "2",
+  "url": "LT6.html#LT6-4-2",
+  "type": "Figure",
+  "number": "12",
+  "title": "",
+  "body": " LT6 Exercises   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
